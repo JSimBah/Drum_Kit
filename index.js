400e90a917b0687d606a6fde63447bc321a74c7b
Jio (Geo) <reppjio@gmail.com>
2023-11-20 01:06:35 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+
+var numBtn = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numBtn; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}  
+    //Detecting Button Press
+    function handleClick() {
+        var buttonHtml = this.innerHTML;
+        makeSound(buttonHtml);
+        buttonAnimation(buttonHtml);
+        
+    }
+    //Detecting Keyboard Press
+    document.addEventListener("keydown", function(event) {
+        makeSound(event.key);
+        buttonAnimation(event.key);
+    });
+
+
+function makeSound(key){
+    switch(key) {
+        case "w":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "a":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        default: console.log(buttonHtml);
+
+    };
+}
+function buttonAnimation(currentKey){
+    var activeBtn = document.querySelector("." + currentKey);
+    activeBtn.classList.add("pressed");
+    setTimeout(function(){
+        activeBtn.classList.remove("pressed");
+    }, 100)
+    
+
+}
+
+
+
+
+
